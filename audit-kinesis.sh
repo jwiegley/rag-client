d2a0fa25aba8b6d69a1e9b22dcf98ae857cd2165
John Wiegley <johnw@newartisans.com>
2025-03-09 12:04:31 -0700

    fix script

--- audit-kinesis.sh
+++ audit-kinesis.sh
@@ -4,5 +4,5 @@ KINESIS=$HOME/kadena/kinesis/smart-contracts/pact
 
 find $KINESIS -name '*.pact' -type f -print | while read file; do
     echo "$file" >> ~/Desktop/audit.md
-    python3 ./ai.py pact.yaml $file ~/Desktop/audit.md
+    python3 ./ai.py pact.yaml $file >> ~/Desktop/audit.md
 done
